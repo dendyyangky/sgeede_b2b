4a316daac1a9125b8a02cb2ab08a456ac402fcbd
root <dendyyangky@gmail.com>
2018-05-31 11:04:32 +0700

    Add License

--- sgeede_b2b/__manifest__.py
+++ sgeede_b2b/__manifest__.py
@@ -1,30 +1,26 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Back to Back Purchase Order from Sales order",
-
-    'summary': """Generate Back to Back Purchase Order in Sale Order""",
-
-    'description': """
-    Generate Back to Back Purchase Order in Sale Order
-        
-    """,
-    
-    'author': "SGEEDE",
-    'website': "http://www.sgeede.com",
+    'name': 'Back to Back Purchase Order from Sales order',
+    'summary': '''Generate Back to Back Purchase Order in Sale Order''',
+    'description': '''
+        Generate Back to Back Purchase Order in Sale Order
+    ''',
+    'author': 'SGEEDE',
+    'website': 'http://www.sgeede.com',
     'category': 'Tools',
     'version': '2',
     'depends': ['base','sale','purchase'],
     'data': [
         'wizard/back_to_back_order_view.xml',
-        'views/sale_view.xml',
-        # 'security/ir.model.access.csv',  
+        'views/sale_view.xml'
     ],
     'demo': [],
     'installable': True,
-    'active': False,
-    'certificate': '',
+    'license': 'LGPL-3',
     'price': 9.99,
     'currency': 'EUR',
-    'images': ['images/main_screenshot.png',
-    'images/sgeede.png'],
+    'images': [
+        'images/main_screenshot.png',
+        'images/sgeede.png'
+    ]
 }
