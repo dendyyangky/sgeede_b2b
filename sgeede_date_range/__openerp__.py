abdb8bd8562fbccf9858c718572305be3e7f09f8
root <dendyyangky@gmail.com>
2018-10-08 16:19:30 +0700

    Change Price to Free

--- sgeede_date_range/__openerp__.py
+++ sgeede_date_range/__openerp__.py
@@ -1,28 +1,22 @@
 # -*- encoding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
     "name": "SGEEDE Date Range",
     "version": "1.0",
     "category": "Custom Development",
-    "summary": """SGEEDE Date Range, Add extra feature to your search filter !""",
-    "description": """SGEEDE Date Range, Add extra feature to your search filter !""",
+    "summary": """SGEEDE Date Range, Add extra feature to your search filter""",
+    "description": """SGEEDE Date Range, Add extra feature to your search filter""",
     "author": "SGEEDE",
     "website": "http://www.sgeede.com",
-    "depends": ['web'],
+    "depends": ["web"],
     "init_xml": [],
-    'data': [
-        'views/backend.xml'
+    "data": [
+        "views/backend.xml"
     ],
-    'qweb': ['static/src/xml/*.xml'],
-    'demo_xml': [],
-    'installable': True,
-    'active': False,
-    'price': 14.99,
-    'currency': 'EUR',
-    'application': True,
-    'images': [
-        'images/main_screenshot.png',
-        'sgeede.png'
+    "qweb": [],
+    "demo_xml": [],
+    "installable": True,
+    "images": [
+        "images/main_screenshot.png",
+        "images/sgeede.png"
     ],
 }
