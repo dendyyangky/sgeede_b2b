e084d7dfe9ba08c5c8e95acee0efaa360179ded5
Ivan <dendyyangky@gmail.com>
2020-01-09 14:27:08 +0700

    Apply Minimum Price Validation on Customer Inovices Only

--- sgeede_minimum_price/models/account_invoice.py
+++ sgeede_minimum_price/models/account_invoice.py
@@ -9,9 +9,10 @@ class account_invoice(models.Model):
 	@api.multi
 	def check_min_price(self):
 		for o in self:
-			if not o.invoice_line:
-				return False
-			for line in o.invoice_line:
-				if line.product_id and (line.price_unit < line.product_id.product_tmpl_id.minimum_price):
-					raise except_orm(_('Error'), _("Price is lower than the minimum product price! \n Please recheck %s") % (line.product_id.name))
+			if o.type == 'out_invoice':
+				if not o.invoice_line:
+					return False
+				for line in o.invoice_line:
+					if line.product_id and (line.price_unit < line.product_id.product_tmpl_id.minimum_price):
+						raise except_orm(_('Error'), _("Price is lower than the minimum product price! \n Please recheck %s") % (line.product_id.name))
 		return True
