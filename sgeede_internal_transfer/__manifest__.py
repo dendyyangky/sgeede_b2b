d1c49ef13e7ee26dd619fd37d01f0b8e0f512e51
Ivan <ivan.yangkaivan@gmail.com>
2020-06-02 17:26:34 +0700

    SGEEDE Internal Transfer V12

---
+++ sgeede_internal_transfer/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': "SGEEDE Internal Transfer",
+    'version': '1.1',
+    'category': 'Tools',
+    'summary': """Odoo's enchanced advance stock internal transfer module""",
+    'description': """Odoo's enchanced advance stock internal transfer module""",
+    'author': 'SGEEDE',
+    'website': 'http://www.sgeede.com',
+    'depends': ['account','stock'],
+    'data': [
+        'data/ir_sequence.xml',
+        'security/ir.model.access.csv',
+        'views/stock_internal_transfer_view.xml',
+        'views/stock_view.xml',
+        'views/res_config_view.xml',
+        'wizard/wizard_stock_internal_transfer_view.xml',
+    ],
+    'qweb': ['static/src/xml/*.xml'],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+    'price': 19.99,
+    'currency': 'EUR',
+    'images': [
+        'images/main_screenshot.png',
+        'images/sgeede.png'
+    ],
+}
---
+++ sgeede_internal_transfer/wizard/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+from . import stock_transfer
+from . import wizard_stock_internal_transfer
